5d48daf40a013934d036f799afdb5c0dad6d4ae0
Daryl Marco <darmarco@gmail.com>
2020-02-25 21:26:36 -0800

    Added sortation, prettier JSON settings

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
 var express = require("express");
-var fetch = require('node-fetch');
-var Promise = require("bluebird");
-fetch.Promise = Promise;
+var fetch = require("node-fetch");
 var app = express();
 var port = 3333;
 
+app.set("json spaces", 2);
+
 //Route 1: Ping
 app.get('/api/ping', (req, res, next) => {
-  res.send({
-    "success": true
+  res.status(200)
+    .json({
+    success: true
    });
-  res.end(200);
-})
+});
 
 //Route 2: Get Posts
-const sortByValidation = ['id', 'reads', 'likes', 'popularity'];
-const directionValidation = ['asc', 'desc'];
-
 app.get('/api/posts', (req, res, next) => {
+  const sortByValidation = ['id', 'reads', 'likes', 'popularity'];
+  const directionValidation = ['asc', 'desc'];
   if (!req.query.tags || req.query.tags === '') {
-    res.send({
-      "error": "Tags parameter is required"
-    });
-    res.end(400);
+    res.status(400)
+      .json({
+      error: "Tags parameter is required"
+    })
     return;
   }
   if (req.query.sortBy && sortByValidation.indexOf(req.query.sortBy) === -1) {
-    res.send({
+    res.status(400)
+      .json({
       "error": "sortBy parameter is invalid"
-    });
-    res.end(400);
+    })
     return;
   }
   if (req.query.direction && directionValidation.indexOf(req.query.direction) === -1) {
-    res.send({
+    res.status(400)
+      .json({
       "error": "direction parameter is invalid"
-    });
-    res.end(400);
+    })
     return;
   }
 
   var tagsParsed = req.query.tags.split(',');
+  var sortBy;
+  req.query.sortBy ? sortBy = req.query.sortBy : sortBy = 'id';
+  var aGTB = 1; //for sortation a greater than b
+  var aLTB = -1; //for sortation, a less than b
+  if (req.query.direction === 'desc') {
+    aGTB = -1;
+    aLTB = 1;
+  }
   var result = []; //collected records returned from API
   var resultIds = []; //ids collected to prevent duplicates; not used for single tag
   var resultArr=[]; //array of promises returned when fetching records
@@ -61,13 +68,26 @@ app.get('/api/posts', (req, res, next) => {
           }
         }
       })
+      .catch(err => new Error())
       )
 
   }
 
-  Promise.all(resultArr).then( function() {
-    res.send({posts: result});
-    res.end(200);
+  Promise.all(resultArr).then(function() {
+    return result.sort((a,b) => {
+      if (a[sortBy] > b[sortBy]) {
+        return aGTB;
+      }
+      if (a[sortBy] === b[sortBy]) {
+        return 0;
+      }
+      return aLTB;
+    })
+  })
+  .then( function(results) {
+    res.status(200)
+      .json({posts: results})
+      .end();
   })
 })
 
