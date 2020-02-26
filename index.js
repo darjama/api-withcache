94a0ae2c3d730981e8d1f642338aec78124f93b0
Daryl Marco <darmarco@gmail.com>
2020-02-25 16:55:11 -0800

    Added node-fetch and bluebird and completed fetch step

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require("express");
+var fetch = require('node-fetch');
+var Promise = require("bluebird");
+fetch.Promise = Promise;
 var app = express();
 var port = 3333;
 
@@ -37,10 +40,35 @@ app.get('/api/posts', (req, res, next) => {
     return;
   }
 
-  res.send({
-    "success": true
-   });
-   res.end(200);
+  var tagsParsed = req.query.tags.split(',');
+  var result = []; //collected records returned from API
+  var resultIds = []; //ids collected to prevent duplicates; not used for single tag
+  var resultArr=[]; //array of promises returned when fetching records
+  for (let i = 0; i < tagsParsed.length; i ++) {
+    resultArr.push(
+    fetch('https://hatchways.io/api/assessment/blog/posts?tag=' + tagsParsed[i])
+      .then(response => response.json())
+      .then(data => {
+        if (tagsParsed.length === 1) {
+          result = data.posts;
+        } else {
+          for (var j = 0; j < data.posts.length; j++) {
+
+            if (resultIds.indexOf(data.posts[j].id === -1)) {
+              resultIds.push(data.posts[j].id);
+              result.push(data.posts[j]);
+            }
+          }
+        }
+      })
+      )
+
+  }
+
+  Promise.all(resultArr).then( function() {
+    res.send({posts: result});
+    res.end(200);
+  })
 })
 
 app.listen(port, () => {
